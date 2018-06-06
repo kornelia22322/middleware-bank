195fb0005dd9f35de82ffc3143e431685897e15c
korneliarohulko <korneliarohulko@gmail.com>
2018-06-06 12:21:00 +0100

    Add subscribers

--- CurrencyServer/src/index.js
+++ CurrencyServer/src/index.js
@@ -6,10 +6,39 @@ const PORT = 50001;
 
 const currencies_proto = grpc.load(PROTO_PATH).currencies;
 
-const supportedCurrencies = ['USD', 'EUR', 'PLN'];
-const subscribersforCurrency = [];
+const supportedCurrencies = {'USD' : 1, 'EUR' : 1, 'CHF' : 1};
+const subscribersforCurrency = {};
 
-console.log(`Currency server on starting port ${PORT}`);
+let bankId;
+
+function calculateCurrenciesRates() {
+  Object.entries(currencies).forEach(([currency, rate]) => {
+      currencies[currency] = Math.random() + 3;
+      notifySubscribers(currency);
+  });
+}
+
+function simulateCurrenciesRates(interval, func) {
+  setInterval(func, interval);
+}
+
+function notifySubscribers(currency) {
+  console.log(`Notifying subscribers about ${currency} rate changed`);
+  subscribers[currency].forEach(subscriber => {
+    try {
+      subscriber.write({currencyRate: {currencyRate: currencies[currency], currencyName: currency}})
+    } catch(_) {
+      console.log('Error while sending message to bank. Removing it from subscribers');
+      removeSubscriber(subscriber);
+    }
+  })
+}
+
+function getEntries() {
+  return Object
+    .entries(currencies)
+    .map(([currencyName, currencyRate]) => ({ currencyRate, currencyName }));
+}
 
 function currencyRates(call) {
   const bankId = id++;
@@ -35,9 +64,12 @@ function currencyRates(call) {
   });
 }
 
+console.log(`Currency server on starting port ${PORT}`);
 const server = new grpc.Server();
 /* addService(service, implementation)
 Add a service to the server, with a corresponding implementation. */
 server.addService(currencies_proto.Currencies.service, { currencyRates });
 server.bind(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure());
 server.start();
+calculateCurrenciesRates();
+simulateCurrenciesRates(3000, calculateCurrenciesRates);
